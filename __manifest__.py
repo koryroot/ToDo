9da028ce85b00f43a8e72bc416ef1287456f9a44
koryroot <20197914@itla.edu.do>
2022-11-16 11:00:14 -0400

    rename somenthing

--- __manifest__.py
+++ __manifest__.py
@@ -25,8 +25,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/task_views.xml',
+
     ],
     # only loaded in demonstration mode
     'demo': [
